7852c0db1e87cb985703fb8d05564fdba9ef0d29
hanan omar altobasi <hananaltobase96@gmail.com>
2021-02-23 16:32:09 +0200

    create file app.js, take user input and compaire this value

---
+++ app.js
@@ -0,0 +1,29 @@
+//take input from user
+//compaire value with he existing variable
+//provide action based on the provided input
+
+var type="camellia";
+var color="red";
+
+var nameUserInput=prompt("What's Your Name?");
+var typeUserInput=prompt("what's your favorite type of rose?");
+var colorUserInput=prompt("what's your favorite color of rose?");
+
+if(nameUserInput===name){
+console.log("that's cute name");
+} else {
+    console.log("that's not correct");
+}
+if(type===typeUserInput){
+    console.log("it is available at our store");
+    } else {
+        console.log("we apologize,this type does not exist");
+    }
+    if(colorUserInput===color){
+        console.log("that's kind color");
+        } else {
+            console.log("that's not correct");
+        }
+            confirm("thank's, your welcom!");
+
+
