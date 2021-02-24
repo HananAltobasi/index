a963057ee019115006f3543415a265876ad14043
hanan omar altobasi <hananaltobase96@gmail.com>
2021-02-24 17:05:18 +0200

    create new file and loop

--- app.js
+++ app.js
@@ -2,28 +2,54 @@
 //compaire value with he existing variable
 //provide action based on the provided input
 
-var type="camellia";
-var color="red";
+//var type="camellia";
+//var color="red";
 
-var nameUserInput=prompt("What's Your Name?");
-var typeUserInput=prompt("what's your favorite type of rose?");
-var colorUserInput=prompt("what's your favorite color of rose?");
+//var nameUserInput=prompt("What's Your Name?");
+//var typeUserInput=prompt("what's your favorite type of rose?");
+//var colorUserInput=prompt("what's your favorite color of rose?");
 
-if(nameUserInput===name){
-console.log("that's cute name");
-} else {
-    console.log("that's not correct");
-}
-if(type===typeUserInput){
-    console.log("it is available at our store");
-    } else {
-        console.log("we apologize,this type does not exist");
-    }
-    if(colorUserInput===color){
-        console.log("that's kind color");
-        } else {
-            console.log("that's not correct");
+//if(nameUserInput===name){
+//console.log("that's cute name");
+//} else {
+  //  console.log("that's not correct");
+//}
+//if(type===typeUserInput){
+  //  console.log("it is available at our store");
+    //} else {
+      //  console.log("we apologize,this type does not exist");
+    //}
+    //if(colorUserInput===color){
+      //  console.log("that's kind color");
+        //} else {
+          //  console.log("that's not correct");
+        //}
+            
+        alert("welcom to Rases Website");
+        while(phoneNumber=="" || phoneNumber==null){
+            phoneNumber=prompt("Please insert your phoneNumber!!");
+            console.log(phoneNumber);
+        }
+        prompt("are you ready");
+        prompt("please choose number 1-5 ^_^");
+              
+        for(var R=1;R<=5;R++){
+            document.write("<img src='https://cdn.statically.io/img/www.qallwdall.com/f=auto%2Cq=60/wp-content/uploads/2020/02/%D8%B5%D9%88%D8%B1-%D9%88%D8%B1%D8%AF-%D8%B1%D9%88%D9%85%D8%A7%D9%86%D8%B3%D9%8A%D8%A9-1.jpg'/>");
+
+        }
+
+          var namberRoses=prompt("How many roses do you want to buy?");
+        if (namberRoses==5 && namberRoses<=10){
+            confirm("you got 10% off *-*");
         }
             confirm("thank's, your welcom!");
+
+
+
+
+
+
+
+
 
 
